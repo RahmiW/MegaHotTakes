699ca88422bced3dfaa37cbe3bf1bf140a30bb8a
RahmiW <abdulrahmanwardere@gmail.com>
2026-01-04 17:23:20 -0600

    Added the Comment Entity

---
+++ src/main/java/org/example/megahottakes/entities/Comment.java
@@ -0,0 +1,35 @@
+package org.example.megahottakes.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+// Who wrote it
+// When was it written
+//
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "hottake_id", nullable = false)
+    private HotTake hotTake;
+}
