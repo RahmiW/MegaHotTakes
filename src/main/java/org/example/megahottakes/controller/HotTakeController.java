5798e842e2f7732469db9d5f49e63fa33192a025
RahmiW <abdulrahmanwardere@gmail.com>
2026-01-12 01:26:07 -0600

    Added HotTakeController

---
+++ src/main/java/org/example/megahottakes/controller/HotTakeController.java
@@ -0,0 +1,64 @@
+package org.example.megahottakes.controller;
+
+
+import org.example.megahottakes.entities.HotTake;
+import org.example.megahottakes.services.HotTakeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/hotTake")
+public class HotTakeController {
+    private final HotTakeService hotTakeService;
+
+    public HotTakeController(HotTakeService hotTakeService) {
+        this.hotTakeService = hotTakeService;
+    }
+
+    // Create
+    @PostMapping("/user/{id}")
+    public void createHotTakePost(@PathVariable Long id, @RequestBody HotTake hotTake) {
+        hotTakeService.createHotTake(id, hotTake.getContent());
+    }
+    // Read
+    @GetMapping("/{id}")
+    public HotTake getHotTake(@PathVariable Long id){
+        return hotTakeService.getHotTake(id);
+    }
+    @GetMapping("/{id}/hottakes")
+    public Set<HotTake> getHotTakeById(@PathVariable Long id){
+        return hotTakeService.getHotTakesByUser(id);
+    }
+    @GetMapping("/feed")
+    public List<HotTake> hotTakeFeed() {
+        return hotTakeService.getHotTakeFeed();
+    }
+    @GetMapping("/search")
+    public List<HotTake> searchHotTakeFeed(@RequestParam String keyword) {
+        return hotTakeService.searchHotTakes(keyword);
+    }
+    @GetMapping("/{id}/heatscore")
+    public Integer getHeatScore(@PathVariable Long id){
+        return hotTakeService.getHeatScore(id);
+    }
+    // Update
+    @PutMapping("/{id}")
+    public HotTake updateHotTake(@PathVariable Long id, @RequestBody HotTake hotTake) {
+        return hotTakeService.updateHotTake(id, hotTake.getContent());
+    }
+    @PatchMapping("/{hotTakeId}/like/{userId}")
+    public int likeHotTake(@PathVariable Long hotTakeId, @PathVariable Long userId) {
+        return hotTakeService.addToHeatScore(userId, hotTakeId);
+    }
+    // Delete
+    @DeleteMapping("/{id}")
+    public void deleteHotTake(@PathVariable Long id){
+        hotTakeService.deleteHotTake(id);
+    }
+    @DeleteMapping("/{hotTakeId}/like/{userId}")
+    public int unlikeHotTake(@PathVariable Long hotTakeId, @PathVariable Long userId) {
+        return hotTakeService.decreaseHeatScore(userId, hotTakeId);
+    }
+}
