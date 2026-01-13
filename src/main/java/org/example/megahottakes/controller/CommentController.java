9cdd9fc323248caa6088ca7bfff495c1c1ead59c
RahmiW <abdulrahmanwardere@gmail.com>
2026-01-13 00:52:44 -0600

    Added HotTakeController

---
+++ src/main/java/org/example/megahottakes/controller/CommentController.java
@@ -0,0 +1,15 @@
+package org.example.megahottakes.controller;
+
+
+import org.example.megahottakes.services.CommentService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+    private CommentService commentService;
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+}
