2a35a1fe51555b855d323aa44e37a5ce42bb18e0
RahmiW <abdulrahmanwardere@gmail.com>
2026-01-14 15:27:08 -0600

    createComment endpoint added

--- src/main/java/org/example/megahottakes/controller/CommentController.java
+++ src/main/java/org/example/megahottakes/controller/CommentController.java
@@ -1,9 +1,9 @@
 package org.example.megahottakes.controller;
 
 
+import org.example.megahottakes.entities.Comment;
 import org.example.megahottakes.services.CommentService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/comments")
@@ -11,5 +11,10 @@ public class CommentController {
     private CommentService commentService;
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
+    }
+    // Create
+    @PostMapping("/hottake/{hotTakeId}/user/{userId}")
+    public Comment createComment(@PathVariable Long hotTakeId, @PathVariable Long userId, @RequestBody Comment comment) {
+        return commentService.addComment(hotTakeId, userId, comment.getContent());
     }
 }
