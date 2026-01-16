13e5eb8d4691539de04de8a2b523575689a9cc5b
RahmiW <abdulrahmanwardere@gmail.com>
2026-01-15 18:18:11 -0600

    added getAllComments for Controller

--- src/main/java/org/example/megahottakes/controller/CommentController.java
+++ src/main/java/org/example/megahottakes/controller/CommentController.java
@@ -5,6 +5,9 @@ import org.example.megahottakes.entities.Comment;
 import org.example.megahottakes.services.CommentService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Set;
+
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
@@ -16,5 +19,10 @@ public class CommentController {
     @PostMapping("/hottake/{hotTakeId}/user/{userId}")
     public Comment createComment(@PathVariable Long hotTakeId, @PathVariable Long userId, @RequestBody Comment comment) {
         return commentService.addComment(hotTakeId, userId, comment.getContent());
+    }
+    // Read
+    @GetMapping("/{id}/all")
+    public Set<Comment> getAllComments(@PathVariable Long id){
+        return commentService.getCommentsByHotTake(id);
     }
 }
