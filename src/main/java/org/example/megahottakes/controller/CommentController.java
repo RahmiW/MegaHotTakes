8b66877e92a509e086e92384c6b12961466f4c78
RahmiW <abdulrahmanwardere@gmail.com>
2026-01-16 01:16:18 -0600

    added deleteCommentById to Controller

--- src/main/java/org/example/megahottakes/controller/CommentController.java
+++ src/main/java/org/example/megahottakes/controller/CommentController.java
@@ -5,24 +5,31 @@ import org.example.megahottakes.entities.Comment;
 import org.example.megahottakes.services.CommentService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Set;
 
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
     private CommentService commentService;
+
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
     }
+
     // Create
     @PostMapping("/hottake/{hotTakeId}/user/{userId}")
     public Comment createComment(@PathVariable Long hotTakeId, @PathVariable Long userId, @RequestBody Comment comment) {
         return commentService.addComment(hotTakeId, userId, comment.getContent());
     }
+
     // Read
     @GetMapping("/{id}/all")
-    public Set<Comment> getAllComments(@PathVariable Long id){
+    public Set<Comment> getAllComments(@PathVariable Long id) {
         return commentService.getCommentsByHotTake(id);
+    }
+    // Delete
+    @DeleteMapping("/{id}")
+    public void deleteCommentById(@PathVariable Long id){
+        commentService.deleteComment(id);
     }
 }
